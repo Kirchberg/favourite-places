1a8052e3bf689fbcefedd5121be235e52f491cc2
Kirchberg <kirill.kostarev.2017@gmail.com>
2020-08-29 20:34:02 +0300

    Added mapViewController

---
+++ FavouritePlaces/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  FavouritePlaces
+//
+//  Created by Kirill Kostarev on 29.08.2020.
+//  Copyright © 2020 Kostarev Kirill Pavlovich. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
